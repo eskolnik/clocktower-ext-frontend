eb840c89963ee6c1082fbd0dd9cac6939bb2582a
Ezra Skolnik <ezraskolnik@gmail.com>
2021-08-14 02:03:15 -0400

    Get baseline config working

--- config.js
+++ config.js
@@ -2,7 +2,9 @@
 
 // TODO: try allowing the streamer to upload a screencap of their stream as a background image for alignment
 
-const MAX_TOKEN_SIZE = 17
+// import MoonMap from "./moonmap/moonMap";
+
+const MAX_TOKEN_SIZE = 17;
 const MIN_TOKEN_SIZE = 8;
 
 const MAX_RADIUS = 400;
@@ -13,33 +15,40 @@ const WINDOW_MAX = 100;
 
 const MOON_CLASS = "clockToken";
 
-
 let state = {
     players: 12,
     radius: 210,
     x: 50,
     y: 50,
-    tokenSize: 10
-}
+    tokenSize: 10,
+};
 
 function validateConfig(config) {
-    if(typeof config.players !== 'number' || config.players < 0) {
-        return false
-    }
-
-    if(typeof config.radius !== 'number' || config.radius < MIN_RADIUS || config.radius > MAX_RADIUS) { 
+    if (typeof config.players !== "number" || config.players < 0) {
         return false;
     }
 
-    if(typeof config.tokenSize !== 'number' || config.tokenSize < MIN_TOKEN_SIZE || config.tokenSize > MAX_TOKEN_SIZE) {
+    if (
+        typeof config.radius !== "number" ||
+    config.radius < MIN_RADIUS ||
+    config.radius > MAX_RADIUS
+    ) {
         return false;
     }
 
-    if(typeof config.x !== 'number' || config.x < 0 || config.x > WINDOW_MAX) { 
+    if (
+        typeof config.tokenSize !== "number" ||
+        config.tokenSize < MIN_TOKEN_SIZE ||
+        config.tokenSize > MAX_TOKEN_SIZE
+    ) {
         return false;
     }
 
-    if(typeof config.y !== 'number' || config.y < 0 || config.y > WINDOW_MAX) { 
+    if (typeof config.x !== "number" || config.x < 0 || config.x > WINDOW_MAX) {
+        return false;
+    }
+
+    if (typeof config.y !== "number" || config.y < 0 || config.y > WINDOW_MAX) {
         return false;
     }
     return true;
@@ -47,151 +56,199 @@ function validateConfig(config) {
 
 const twitch = window.Twitch.ext;
 
-console.log(twitch.configuration.broadcaster)
 
-// Overwrite options with twitch config if present
-if(twitch.configuration.broadcaster) {
+const log = (...args) => {
+    console.log(...args);
+    twitch.rig.log(...args);
+};
+
+const updateStateFromConfig = (data) => {
+    log(data);
     try {
-        let config = JSON.parse(twitch.configuration.broadcaster.content);
+        let config = JSON.parse(data);
+        log("parsed config: ", config);
 
-        if(typeof config === 'object' && validateConfig(config)) {
+        if (typeof config === "object" && validateConfig(config)) {
+            log("validated config");
             // overwrite default state with config from twitch
             state = config;
-        } else {
-            console.log('Invalid Config')
+            moveCenter();
+            destroyOverlay();
+            createOverlay();
+        }
+        else {
+            log("Invalid Config", config);
         }
     } catch (err) {
-        console.log('Invalid Config')
+        log("Unable to parse config", err);
     }
-}
+};
 
+twitch.onAuthorized((auth) => {
+    let { token, userId } = auth;
+    log("Auth: ", auth);
+
+    try {
+        updateStateFromConfig(twitch.configuration.broadcaster.content);
+    } catch (err) {
+        log(err);
+    }
+
+    /**
+   * Update values on change
+   */
+    twitch.configuration.onChanged(() => {
+        log("config changed");
+        if (twitch.configuration.broadcaster) {
+            updateStateFromConfig(twitch.configuration.broadcaster.content);
+        }
+    });
+});
 
 /**
  * Update the Twitch Config Service with the current values in state
  */
 function updateConfig() {
-    twitch.configuration.set('broadcaster', '1', JSON.stringify(state));
+    twitch.configuration.set("broadcaster", "1", JSON.stringify(state));
 }
 
 // Core positioning logic
 function createOverlay() {
-
     new MoonMap("#center", {
         n: state.players,
         radius: state.radius,
         moonClass: `${MOON_CLASS} ${MOON_CLASS}-${state.tokenSize}`,
-        content: (i) => `<div class="ability">Reminder text</div>`,
+        content: (i) => "<div class=\"ability\">Reminder text</div>",
     });
 }
 
 function destroyOverlay() {
-    let moons = Array.from(document.getElementsByClassName(MOON_CLASS))
-    moons.forEach(moon => moon.parentNode.removeChild(moon))
+    let moons = Array.from(document.getElementsByClassName(MOON_CLASS));
+    moons.forEach((moon) => moon.parentNode.removeChild(moon));
 }
 
 function moveCenter() {
-    const center = document.getElementById("center")
-    center.style.top = `${state.y}%`
-    center.style.left = `${state.x}%`
-
+    const center = document.getElementById("center");
+    center.style.top = `${state.y}%`;
+    center.style.left = `${state.x}%`;
 }
 
-function handleClickUp () {
+function handleClickUp() {
     state.y = state.y <= 0 ? 0 : state.y - 1;
-    moveCenter(state.x, state.y)
+    moveCenter(state.x, state.y);
 }
-document.getElementById("button-up").addEventListener("click", handleClickUp)
+document.getElementById("button-up").addEventListener("click", handleClickUp);
 
-function handleClickDown () {
+function handleClickDown() {
     state.y += 1;
-    moveCenter(state.x, state.y)
+    moveCenter(state.x, state.y);
 }
-document.getElementById("button-down").addEventListener("click", handleClickDown)
+document
+    .getElementById("button-down")
+    .addEventListener("click", handleClickDown);
 
-function handleClickLeft () {
+function handleClickLeft() {
     state.x = state.x <= 0 ? 0 : state.x - 1;
-    moveCenter(state.x, state.y)
+    moveCenter(state.x, state.y);
 }
-document.getElementById("button-left").addEventListener("click", handleClickLeft)
+document
+    .getElementById("button-left")
+    .addEventListener("click", handleClickLeft);
 
-function handleClickRight () {
+function handleClickRight() {
     state.x += 1;
-    moveCenter(state.x, state.y)
+    moveCenter(state.x, state.y);
 }
-document.getElementById("button-right").addEventListener("click", handleClickRight)
-
+document
+    .getElementById("button-right")
+    .addEventListener("click", handleClickRight);
 
 function handleAddPlayer() {
-    state.players +=1;
+    state.players += 1;
     destroyOverlay();
-    createOverlay(state.players, state.radius)
+    createOverlay(state.players, state.radius);
 }
-document.getElementById("button-add").addEventListener("click", handleAddPlayer)
+document
+    .getElementById("button-add")
+    .addEventListener("click", handleAddPlayer);
 
 function handleRemovePlayer() {
-    state.players = state.players <= 0 ? 0 : state.players -1;
+    state.players = state.players <= 0 ? 0 : state.players - 1;
 
     destroyOverlay();
-    createOverlay(state.players, state.radius)
+    createOverlay(state.players, state.radius);
 }
-document.getElementById("button-remove").addEventListener("click", handleRemovePlayer)
+document
+    .getElementById("button-remove")
+    .addEventListener("click", handleRemovePlayer);
 
 function handleBiggerToken() {
-    state.tokenSize = state.tokenSize >= MAX_TOKEN_SIZE ? MAX_TOKEN_SIZE : state.tokenSize + 1;
+    state.tokenSize =
+    state.tokenSize >= MAX_TOKEN_SIZE ? MAX_TOKEN_SIZE : state.tokenSize + 1;
 
     destroyOverlay();
-    createOverlay(state.players, state.radius)
+    createOverlay(state.players, state.radius);
 }
-document.getElementById("button-bigger").addEventListener("click", handleBiggerToken)
+document
+    .getElementById("button-bigger")
+    .addEventListener("click", handleBiggerToken);
 
 function handleSmallerToken() {
-    state.tokenSize = state.tokenSize <= MIN_TOKEN_SIZE ? MIN_TOKEN_SIZE : state.tokenSize - 1;
+    state.tokenSize =
+    state.tokenSize <= MIN_TOKEN_SIZE ? MIN_TOKEN_SIZE : state.tokenSize - 1;
 
     destroyOverlay();
-    createOverlay(state.players, state.radius)
+    createOverlay(state.players, state.radius);
 }
-document.getElementById("button-smaller").addEventListener("click", handleSmallerToken)
+document
+    .getElementById("button-smaller")
+    .addEventListener("click", handleSmallerToken);
 
 function handleIncreaseRadius() {
-    state.radius = state.radius >= MAX_RADIUS ? MAX_RADIUS : state.radius + RADIUS_INCREMENT;
+    state.radius =
+    state.radius >= MAX_RADIUS ? MAX_RADIUS : state.radius + RADIUS_INCREMENT;
 
     destroyOverlay();
-    createOverlay(state.players, state.radius)
+    createOverlay(state.players, state.radius);
 }
-document.getElementById("button-expand").addEventListener("click", handleIncreaseRadius)
+document
+    .getElementById("button-expand")
+    .addEventListener("click", handleIncreaseRadius);
 
 function handleDecreaseRadius() {
-    state.radius = state.radius <= MIN_RADIUS ? MIN_RADIUS : state.radius - RADIUS_INCREMENT;
+    state.radius =
+    state.radius <= MIN_RADIUS ? MIN_RADIUS : state.radius - RADIUS_INCREMENT;
 
     destroyOverlay();
-    createOverlay(state.players, state.radius)
+    createOverlay(state.players, state.radius);
 }
-document.getElementById("button-contract").addEventListener("click", handleDecreaseRadius)
+document
+    .getElementById("button-contract")
+    .addEventListener("click", handleDecreaseRadius);
 
 let reader = new FileReader();
-reader.addEventListener("load", event => {
+reader.addEventListener("load", (event) => {
     const bg = document.getElementById("bg");
     bg.src = reader.result;
     // document.body.style.background = `url(${reader.result}) no-repeat center center fixed`
-})
+});
 
 function handleSetBackground(e) {
     var file = e.target.files[0];
 
-    if(file){
+    if (file) {
         reader.readAsDataURL(file);
-    }else{
+    } else {
+        //do nothing
     }
 }
 
-document.getElementById("button-background").addEventListener("input", handleSetBackground)
-
+document
+    .getElementById("button-background")
+    .addEventListener("input", handleSetBackground);
 
 // Handle saving settings
-document.getElementById("button-save").addEventListener("click", updateConfig)
-
+document.getElementById("button-save").addEventListener("click", updateConfig);
 
 // Setup initial overlay
-createOverlay(state.players, state.radius)
-
-
+createOverlay(state.players, state.radius);
--- video_overlay.js
+++ video_overlay.js
@@ -1,12 +1,10 @@
 // Control overlay position and role content
 
-// TODO: Pull game state from live game
-// we can pull this from localStorage.players on the host
+// TODO: try allowing the streamer to upload a screencap of their stream as a background image for alignment
 
-// custom scripts can be parsed similarly from localStorage.edition
-// and in fact, maybe we should just do this for all scripts? meh
+// import MoonMap from "./moonmap/moonMap";
 
-const MAX_TOKEN_SIZE = 17
+const MAX_TOKEN_SIZE = 17;
 const MIN_TOKEN_SIZE = 8;
 
 const MAX_RADIUS = 400;
@@ -17,33 +15,40 @@ const WINDOW_MAX = 100;
 
 const MOON_CLASS = "clockToken";
 
-
 let state = {
     players: 12,
     radius: 210,
     x: 50,
     y: 50,
-    tokenSize: 10
-}
+    tokenSize: 10,
+};
 
 function validateConfig(config) {
-    if(typeof config.players !== 'number' || config.players < 0) {
-        return false
-    }
-
-    if(typeof config.radius !== 'number' || config.radius < MIN_RADIUS || config.radius > MAX_RADIUS) { 
+    if (typeof config.players !== "number" || config.players < 0) {
         return false;
     }
 
-    if(typeof config.tokenSize !== 'number' || config.tokenSize < MIN_TOKEN_SIZE || config.tokenSize > MAX_TOKEN_SIZE) {
+    if (
+        typeof config.radius !== "number" ||
+    config.radius < MIN_RADIUS ||
+    config.radius > MAX_RADIUS
+    ) {
         return false;
     }
 
-    if(typeof config.x !== 'number' || config.x < 0 || config.x > WINDOW_MAX) { 
+    if (
+        typeof config.tokenSize !== "number" ||
+        config.tokenSize < MIN_TOKEN_SIZE ||
+        config.tokenSize > MAX_TOKEN_SIZE
+    ) {
         return false;
     }
 
-    if(typeof config.y !== 'number' || config.y < 0 || config.y > WINDOW_MAX) { 
+    if (typeof config.x !== "number" || config.x < 0 || config.x > WINDOW_MAX) {
+        return false;
+    }
+
+    if (typeof config.y !== "number" || config.y < 0 || config.y > WINDOW_MAX) {
         return false;
     }
     return true;
@@ -51,68 +56,83 @@ function validateConfig(config) {
 
 const twitch = window.Twitch.ext;
 
-// Overwrite options with twitch config if present
-if(twitch.configuration.broadcaster) {
-    try {
-        let config = JSON.parse(twitch.configuration.broadcaster.content);
 
-        if(typeof config === 'object' && validateConfig(config)) {
+const log = (...args) => {
+    console.log(...args);
+    twitch.rig.log(...args);
+};
+
+const updateStateFromConfig = (data) => {
+    log(data);
+    try {
+        let config = JSON.parse(data);
+        log("parsed config: ", config);
+
+        if (typeof config === "object" && validateConfig(config)) {
+            log("validated config");
             // overwrite default state with config from twitch
             state = config;
-        } else {
-            console.log('Invalid Config')
+            moveCenter();
+            destroyOverlay();
+            createOverlay();
+        }
+        else {
+            log("Invalid Config", config);
         }
     } catch (err) {
-        console.log('Invalid Config')
+        log("Unable to parse config", err);
     }
-}
+};
+
+twitch.onAuthorized((auth) => {
+    let { token, userId } = auth;
+    log("Auth: ", auth);
+
+    try {
+        updateStateFromConfig(twitch.configuration.broadcaster.content);
+    } catch (err) {
+        log(err);
+    }
+
+    /**
+   * Update values on change
+   */
+    twitch.configuration.onChanged(() => {
+        log("config changed");
+        if (twitch.configuration.broadcaster) {
+            updateStateFromConfig(twitch.configuration.broadcaster.content);
+        }
+    });
+});
 
 /**
- * Update values on change
+ * Update the Twitch Config Service with the current values in state
  */
-twitch.configuration.onChanged(() => {
-    if(twitch.configuration.broadcaster) {
-        try {
-            let config = JSON.parse(twitch.configuration.broadcaster.content);
-    
-            if(typeof config === 'object' && validateConfig(config)) {
-                // overwrite default state with config from twitch
-                state = config;
-                moveCenter();
-                destroyOverlay();
-                createOverlay();
-            } else {
-                console.log('Invalid Config')
-            }
-        } catch (err) {
-            console.log('Invalid Config')
-        }
-    }
-})
+function updateConfig() {
+    twitch.configuration.set("broadcaster", "1", JSON.stringify(state));
+}
 
 // Core positioning logic
 function createOverlay() {
-
     new MoonMap("#center", {
         n: state.players,
         radius: state.radius,
         moonClass: `${MOON_CLASS} ${MOON_CLASS}-${state.tokenSize}`,
-        content: (i) => `<div class="ability">Reminder text</div>`,
+        content: (i) => "<div class=\"ability\">Reminder text</div>",
     });
 }
 
 function destroyOverlay() {
-    let moons = Array.from(document.getElementsByClassName(MOON_CLASS))
-    moons.forEach(moon => moon.parentNode.removeChild(moon))
+    let moons = Array.from(document.getElementsByClassName(MOON_CLASS));
+    moons.forEach((moon) => moon.parentNode.removeChild(moon));
 }
 
 function moveCenter() {
-    const center = document.getElementById("center")
-    center.style.top = `${state.y}%`
-    center.style.left = `${state.x}%`
-
+    const center = document.getElementById("center");
+    center.style.top = `${state.y}%`;
+    center.style.left = `${state.x}%`;
 }
 
-createOverlay(state.players, state.radius)
 
-
+// Setup initial overlay
+createOverlay(state.players, state.radius);
