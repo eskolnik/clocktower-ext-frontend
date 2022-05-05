ce1ffefcdaa642aae8eb445a954e279ee36bff3c
Ezra Skolnik <ezraskolnik@gmail.com>
2022-05-05 10:33:36 -0400

    remove old icon

--- src/bookmarklet/bookmarklet.js
+++ src/bookmarklet/bookmarklet.js
@@ -115,11 +115,6 @@
         
         assignStyles(extensionNode, extensionStyles);
         
-        // const iconNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        // iconNode.setAttribute("width", "40");
-        // iconNode.setAttribute("height", "40");
-        // iconNode.setAttribute("viewBox", "0 0 100 100");
-        // iconNode.innerHTML = "<path d=\"M5.7 0L1.4 10.985V55.88h15.284V64h8.597l8.12-8.12h12.418l16.716-16.716V0H5.7zm51.104 36.3L47.25 45.85H31.967l-8.12 8.12v-8.12H10.952V5.73h45.85V36.3zM47.25 16.716v16.716h-5.73V16.716h5.73zm-15.284 0v16.716h-5.73V16.716h5.73z\" fill=\"#6441a4\" fill-rule=\"evenodd\"></path>";
         const iconNode = document.createElement("div");
         iconNode.innerHTML="CC";
         const iconStyles = {
